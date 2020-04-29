c50c2e44b237dcf1e4ce271f3c36f5efaafd2d17
s1897 <tillheim@bluewin.ch>
2020-04-29 17:01:51 +0200

    .

---
+++ CRSC.c
@@ -0,0 +1,14 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  int num[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+  char answer[20];
+
+  fgets(answer, 20, stdin);
+
+  printf("%s", answer);
+}
